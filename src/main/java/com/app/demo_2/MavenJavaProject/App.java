d31e8220041773ecc315e2d833aeb475789d789f
manideep <vooradimanideep@gmail.com>
2024-11-26 14:56:49 +0530

    mvnj

---
+++ src/main/java/com/app/demo_2/MavenJavaProject/App.java
@@ -0,0 +1,13 @@
+package com.app.demo_2.MavenJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
